e890e27a595c8586ee4c61945aa41d27d00dc48a
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2023-09-20 04:28:58 +0000

    Add files via upload

---
+++ VBox-Undetected-Remove.bat
@@ -0,0 +1,31 @@
+:: ==================================================
+::  VBox-Undetected
+:: ==================================================
+::  Dev  - Scut1ny
+::  Help - 
+::  Link - https://github.com/Scrut1ny/VBox-Undetected
+:: ==================================================
+
+@echo off
+setlocal enableDelayedExpansion
+
+
+:: HKLM\HARDWARE\ACPI
+
+:: DSDT
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\DSDT\VBOX__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\DSDT\ALASKA\VBOXBIOS'" -NewName "'A_M_I_'" -Force
+
+:: FADT
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\FADT\VBOX__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\FADT\ASUS__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\FADT\ALASKA\VBOXFACP'" -NewName "'A_M_I_'" -Force
+
+:: RSDT
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\RSDT\VBOX__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\RSDT\ASUS__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\RSDT\ALASKA\VBOXXSDT'" -NewName "'A_M_I_'" -Force
+
+:: SSDT
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\SSDT\VBOX__'" -NewName "'ALASKA'" -Force
+PowerShell Rename-Item -Path "'HKLM:\HARDWARE\ACPI\SSDT\ALASKA\VBOXCPUT'" -NewName "'A_M_I_'" -Force
