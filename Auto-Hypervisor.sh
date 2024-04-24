9835d45bd931feea02a0ab6f1154e62ed7474c87
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2024-04-24 02:02:10 +0000

    Add files via upload

--- Auto-Hypervisor.sh
+++ Auto-Hypervisor.sh
@@ -85,9 +85,8 @@ function QEMU() {
             # Dependencies & Prerequisites
             echo -e "\n  [+] Installing QEMU Dependencies"
             {
-                sudo apt install -y git python3-venv
-                sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-                sudo apt install -y binutils-mingw-w64 binutils-mingw-w64-i686 binutils-mingw-w64-x86-64 build-essential clang g++-mingw-w64 g++-mingw-w64-i686 g++-mingw-w64-x86-64 gcc-mingw-w64 gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 git git-email gnutls-bin libaio-dev libbluetooth-dev libbrlapi-dev libbz2-dev libcacard-dev libcap-dev libcap-ng-dev libcurl4-gnutls-dev libfdt-dev libglib2.0-dev libgtk-3-dev libibverbs-dev libiscsi-dev libjpeg8-dev liblzo2-dev libncurses5-dev libncursesw5-dev libnfs-dev libnuma-dev libpam0g-dev libpixman-1-dev librbd-dev librdmacm-dev libseccomp-dev libsnappy-dev libsasl2-dev libsdl1.2-dev libsdl2-dev libsdl2-image-dev libspice-protocol-dev libspice-server-dev libusb-1.0-0-dev libusb-dev libusbredirparser-dev libusbredirparser1 libvde-dev libvdeplug-dev libvirglrenderer-dev libvte-2.91-dev libxen-dev libxml2-dev libz-mingw-w64-dev libzstd-dev ninja-build valgrind win-iconv-mingw-w64-dev xfslibs-dev zlib1g-dev
+                sudo apt install -y git python3-venv libglib2.0-0 flex bison
+                sudo apt install -y binutils-mingw-w64 binutils-mingw-w64-i686 binutils-mingw-w64-x86-64 clang g++-mingw-w64 g++-mingw-w64-i686 g++-mingw-w64-x86-64 gcc-mingw-w64 gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 git git-email gnutls-bin libaio-dev libbluetooth-dev libbrlapi-dev libbz2-dev libcacard-dev libcap-dev libcap-ng-dev libcurl4-gnutls-dev libfdt-dev libglib2.0-dev libgtk-3-dev libibverbs-dev libiscsi-dev libjpeg8-dev liblzo2-dev libncurses5-dev libncursesw5-dev libnfs-dev libnuma-dev libpam0g-dev libpixman-1-dev librbd-dev librdmacm-dev libseccomp-dev libsnappy-dev libsasl2-dev libsdl1.2-dev libsdl2-dev libsdl2-image-dev libspice-protocol-dev libspice-server-dev libusb-1.0-0-dev libusb-dev libusbredirparser-dev libusbredirparser1 libvde-dev libvdeplug-dev libvirglrenderer-dev libvte-2.91-dev libxen-dev libxml2-dev libz-mingw-w64-dev libzstd-dev ninja-build valgrind win-iconv-mingw-w64-dev xfslibs-dev zlib1g-dev
                 sudo apt install -y virt-manager libvirt-clients libvirt-daemon-system libvirt-daemon-config-network bridge-utils ovmf
                 sudo usermod -a -G kvm,libvirt $(whoami)
                 sudo systemctl enable libvirtd && sudo systemctl start libvirtd
@@ -118,7 +117,7 @@ function QEMU() {
     } >/dev/null 2>&1
     echo -e "\n  [+] Applying Custom QEMU Patch"
     
-    # Spoofing all serials numbers
+    # Spoofing all USB serial numbers
     echo -e "\n  [+] Spoofing all serial numbers\n"
     find "$(pwd)/hw/usb" -type f -exec grep -l '\[STR_SERIALNUMBER\]' {} + | while IFS= read -r file; do
         NEW_SERIAL=$(tr -dc 'A-Z0-9' </dev/urandom | head -c 10)
@@ -336,7 +335,7 @@ menu() {
                 ;;
             3)
                 clear && echo -e "\n  [!] Not supported yet, in progress."
-                Kernal_Patch
+                # Kernal_Patch
                 ;;
             4)
                 clear && Looking_Glass
