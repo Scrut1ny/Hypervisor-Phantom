f7487ab1f96bbe8216d9d99ac706dc628fbf6b20
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-01-23 22:52:51 +0300

    Update Auto-Hypervisor.sh

--- Hypervisor-Phantom/Auto-Hypervisor.sh
+++ Hypervisor-Phantom/Auto-Hypervisor.sh
@@ -85,14 +85,9 @@ print_system_info() {
 }
 
 check_internet_connection() {
-  # Try to reach AWS IP check service
-  if ! curl -s --head --request GET https://checkip.amazonaws.com | grep "200" > /dev/null; then
-    # If AWS is unreachable, try Cloudflare CDN test page
-    if ! curl -s --head --request GET https://www.cloudflare.com/cdn-cgi/trace | grep "200" > /dev/null; then
-      fmtr::error "No internet connection detected. Please check your network settings."
-      echo -e "\n"
-      exit 1
-    fi
+  if ! ping -c 1 8.8.8.8 &> /dev/null; then
+    fmtr::error "No internet connection."
+    exit 1
   fi
 }
 
