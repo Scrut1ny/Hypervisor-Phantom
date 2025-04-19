73360b69823700e5adeb1570d00f815ee80baf06
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-04-19 19:51:54 +0300

    Update patch_kernel.sh

--- Hypervisor-Phantom/functions/patch_kernel.sh
+++ Hypervisor-Phantom/functions/patch_kernel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://github.com/Frogging-Family/linux-tkg
+
 [[ -z "$DISTRO" || -z "$LOG_FILE" ]] && exit 1
 
 
@@ -15,7 +17,10 @@ readonly TKG_DIR="linux-tkg"
 readonly TKG_CFG_DIR="../../$SRC_DIR/linux-tkg/customization.cfg"
 readonly PATCH_DIR="../../patches/Kernel"
 readonly KERNEL_PATCH="*.mypatch"
-readonly KERNEL_VERSION="6.14.2"
+readonly KERNEL_MAJOR="6"
+readonly KERNEL_MINOR="14"
+readonly KERNEL_PATCH="2"
+readonly KERNEL_VERSION="${KERNEL_MAJOR}.${KERNEL_MINOR}.${KERNEL_PATCH}"
 
 
 acquire_tkg_source() {
@@ -24,7 +29,7 @@ acquire_tkg_source() {
   if [ -d "$TKG_DIR" ]; then
     if [ -d "$TKG_DIR/.git" ]; then
       fmtr::warn "Directory $TKG_DIR already exists and is a valid Git repository."
-      if ! prmt::yes_or_no "$(fmtr::ask 'Delete and re-clone the TKG source?')"; then
+      if ! prmt::yes_or_no "$(fmtr::ask 'Delete and re-clone the linux-tkg source?')"; then
         fmtr::info "Keeping existing directory. Skipping re-clone."
         cd "$TKG_DIR" || { fmtr::fatal "Failed to change to TKG directory after cloning: $TKG_DIR"; exit 1; }
         return
@@ -68,14 +73,13 @@ select_distro() {
         6) distro="Gentoo" ;;
         7) distro="Generic" ;;
         *)
-            clear
-            fmtr::error "Invalid option, please try again."
+            clear; fmtr::error "Invalid option, please try again."
             prmt::quick_prompt "$(fmtr::info 'Press any key to continue...')"
             continue
             ;;
     esac
 
-    echo ""; fmtr::info "You selected: $distro"
+    echo ""
     break
   done
 }
@@ -83,10 +87,71 @@ select_distro() {
 
 modify_customization_cfg() {
 
-    sed -i 's/_distro="[^"]*"/_distro="'"$distro"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
-    sed -i 's/_version="[^"]*"/_version="'"$KERNEL_VERSION"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_distro="[^"]*"/_distro="'"$distro"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_version="[^"]*"/_version="'"$KERNEL_VERSION"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_cpusched="[^"]*"/_cpusched="'"pds"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_compiler="[^"]*"/_compiler="'"gcc"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_sched_yield_type="[^"]*"/_sched_yield_type="'"0"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_rr_interval="[^"]*"/_rr_interval="'"2"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  sed -i 's/_tickless="[^"]*"/_tickless="'"1"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+
+  ###
+
+  fmtr::info "This patch enables corrected IOMMU grouping on
+      motherboards with poor PCIe IOMMU grouping."
+  if prmt::yes_or_no "$(fmtr::ask 'Apply ACS override bypass Kernel patch?')"; then
+      sed -i 's/_acs_override="[^"]*"/_acs_override="'"true"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  else
+      sed -i 's/_acs_override="[^"]*"/_acs_override="'"false"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+  fi
+
+  ###
+
+  sed -i 's/_processor_opt="[^"]*"/_processor_opt="'""'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+
+  ###
+
+  if output=$(/lib/ld-linux-x86-64.so.2 --help 2>/dev/null | grep supported); then
+      :
+  elif output=$(/lib64/ld-linux-x86-64.so.2 --help 2>/dev/null | grep supported); then
+      :
+  fi
+
+  highest=0
+
+  while IFS= read -r line; do
+      if [[ $line =~ x86-64-v([123]) ]]; then
+          version="${BASH_REMATCH[1]}"
+          if (( version > highest )); then
+              highest=$version
+          fi
+      fi
+  done <<< "$output"
+
+  x86_version=$highest
+
+  sed -i 's/_x86_64_isalvl="[^"]*"/_x86_64_isalvl="'"$highest"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+
+  ###
+
+  sed -i 's/_smt_nice="[^"]*"/_smt_nice="'"true"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+
+  ###
+
+  sed -i 's/_timer_freq="[^"]*"/_timer_freq="'""'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
+
+  ###
+
+  sed -i 's/_default_cpu_gov="[^"]*"/_default_cpu_gov="'"performance"'"/' $TKG_CFG_DIR &>> "$LOG_FILE"
 
 }
+
+patch_kernel() {
+
+  mkdir -p "linux${KERNEL_MAJOR}${KERNEL_MINOR}-tkg-userpatches" && cd "linux${KERNEL_MAJOR}${KERNEL_MINOR}-tkg-userpatches"
+
+}
+
 
 
 arch_distro() {
