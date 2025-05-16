a5e8be8327e65af86e214ba983a3cbc4a06d6a1a
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-05-16 11:56:34 +0300

    hotfix

--- Hypervisor-Phantom/functions/spoof_edk2_patch.sh
+++ Hypervisor-Phantom/functions/spoof_edk2_patch.sh
@@ -126,6 +126,8 @@ compile_ovmf() {
         --define TPM2_ENABLE=TRUE \
         &>> "$LOG_FILE"
 
+    sudo mkdir -p "$OVMF_CODE_DEST_DIR"
+
     fmtr::log "Converting compiled OVMF firmware to .qcow2 format..."
     sudo qemu-img convert -f raw -O qcow2 "Build/OvmfX64/RELEASE_GCC5/FV/OVMF_CODE.fd" "$OVMF_CODE_DEST_DIR/OVMF_CODE.secboot.4m.qcow2"
     sudo qemu-img convert -f raw -O qcow2 "Build/OvmfX64/RELEASE_GCC5/FV/OVMF_VARS.fd" "$OVMF_CODE_DEST_DIR/OVMF_CODE_VARS.4m.qcow2"
