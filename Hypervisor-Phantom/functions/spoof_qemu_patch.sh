7c5b08409915cfd0cf60c2f69afa8f8275d73c56
NULLter <118298840+NULLter@users.noreply.github.com>
2025-05-22 02:40:07 +0530

    removed "protable" SSDs
    portable SSDs are boot drives are generally frowned upon

--- Hypervisor-Phantom/functions/spoof_qemu_patch.sh
+++ Hypervisor-Phantom/functions/spoof_qemu_patch.sh
@@ -264,17 +264,16 @@ spoof_drive_serial_number() {
     "Samsung SSD 850 PRO 1TB" "Samsung SSD T7 Touch 1TB"
     "Samsung SSD 840 EVO 1TB" "WD Blue SN570 NVMe SSD 1TB"
     "WD Black SN850 NVMe SSD 1TB" "WD Green 1TB SSD"
-    "WD My Passport SSD 1TB" "WD Blue 3D NAND 1TB SSD"
+    "WD Blue 3D NAND 1TB SSD" "Crucial P3 1TB PCIe 3.0 3D NAND NVMe SSD"
     "Seagate BarraCuda SSD 1TB" "Seagate FireCuda 520 SSD 1TB"
     "Seagate One Touch SSD 1TB" "Seagate IronWolf 110 SSD 1TB"
     "Seagate Fast SSD 1TB" "Crucial MX500 1TB 3D NAND SSD"
     "Crucial P5 Plus NVMe SSD 1TB" "Crucial BX500 1TB 3D NAND SSD"
-    "Crucial X8 Portable SSD 1TB" "Crucial P3 1TB PCIe 3.0 3D NAND NVMe SSD"
     "Kingston A2000 NVMe SSD 1TB" "Kingston KC2500 NVMe SSD 1TB"
     "Kingston A400 SSD 1TB" "Kingston HyperX Savage SSD 1TB"
     "Kingston DataTraveler Vault Privacy 3.0 1TB" "SanDisk Ultra 3D NAND SSD 1TB"
-    "SanDisk Extreme Portable SSD V2 1TB" "SanDisk SSD PLUS 1TB"
-    "SanDisk Ultra 3D 1TB NAND SSD" "SanDisk Extreme Pro 1TB NVMe SSD"
+    "SanDisk SSD PLUS 1TB" "SanDisk Ultra 3D 1TB NAND SSD"
+     "SanDisk Extreme Pro 1TB NVMe SSD"
   )
 
   get_random_element() {
