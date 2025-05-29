4e08a60d1b4527c758f515a642c87c7af6eae0a9
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-05-29 16:48:41 +0300

    Update patch_kernel.sh

--- Hypervisor-Phantom/functions/patch_kernel.sh
+++ Hypervisor-Phantom/functions/patch_kernel.sh
@@ -11,7 +11,7 @@ source "./utils/packages.sh"
 declare -r CPU_VENDOR=$(case "$VENDOR_ID" in
   *AuthenticAMD*) echo "svm" ;;
   *GenuineIntel*) echo "vmx" ;;
-  *) fmtr::error "Unknown CPU vendor."; exit 1 ;;
+  *) fmtr::error "Unknown CPU Vendor ID."; exit 1 ;;
 esac)
 
 readonly SRC_DIR="src"
