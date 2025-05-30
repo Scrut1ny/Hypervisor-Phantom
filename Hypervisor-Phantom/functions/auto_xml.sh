edb0bd10dd87c9e109465b1f11b3c620e51cfb00
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-05-30 23:35:07 +0300

    Update auto_xml.sh

--- Hypervisor-Phantom/functions/auto_xml.sh
+++ Hypervisor-Phantom/functions/auto_xml.sh
@@ -156,10 +156,8 @@ sudo virt-xml "$VM_NAME" --edit --qemu-commandline="
 ##################################################
 ### MAC address randomization
 
-# Generate a fully random, locally administered, unicast MAC address if not preset already.
-if [[ -z "$MAC_ADDRESS" ]]; then
-    MAC_ADDRESS=$(printf '02:%02X:%02X:%02X:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
-fi
+# Generate a fully random, locally administered, unicast MAC address.
+MAC_ADDRESS=$(printf '02:%02X:%02X:%02X:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
 
 # Apply the MAC address to the default network interface using virt-xml
 sudo virt-xml "$VM_NAME" --edit --network network=default,mac="$MAC_ADDRESS" &>> "$LOG_FILE"
