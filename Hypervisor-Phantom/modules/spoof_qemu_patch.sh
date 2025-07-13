7e025f1e13714e444fe6fe2810dbf7582c2842a1
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-07-13 10:14:02 +0300

    Added "patch" package requirement

--- Hypervisor-Phantom/modules/spoof_qemu_patch.sh
+++ Hypervisor-Phantom/modules/spoof_qemu_patch.sh
@@ -28,7 +28,7 @@ readonly FAKE_BATTERY_ACPITABLE="${PATCH_DIR}/fake_battery.dsl"
 REQUIRED_PKGS_Arch=(
   # Basic Build Dependencie(s)
   acpica base-devel dmidecode glib2 ninja python-packaging
-  python-sphinx python-sphinx_rtd_theme gnupg
+  python-sphinx python-sphinx_rtd_theme gnupg patch
 
   # Spice Dependencie(s)
   spice gtk3
@@ -44,7 +44,7 @@ REQUIRED_PKGS_Debian=(
   # Basic Build Dependencie(s)
   acpica-tools build-essential libfdt-dev libglib2.0-dev
   libpixman-1-dev ninja-build python3-venv zlib1g-dev gnupg
-  python3-sphinx python3-sphinx-rtd-theme
+  python3-sphinx python3-sphinx-rtd-theme patch
 
   # Spice Dependencie(s)
   libspice-server-dev
@@ -58,7 +58,7 @@ REQUIRED_PKGS_Debian=(
 
 REQUIRED_PKGS_openSUSE=(
   # Basic Build Dependencie(s)
-  acpica bzip2 gcc-c++ gpg2 glib2-devel make qemu  
+  acpica bzip2 gcc-c++ gpg2 glib2-devel make qemu
   libpixman-1-0-devel patch python3-Sphinx ninja
 
   # Spice Dependencie(s)
@@ -74,7 +74,7 @@ REQUIRED_PKGS_openSUSE=(
 REQUIRED_PKGS_Fedora=(
   # Basic Build Dependencie(s)
   acpica-tools bzip2 glib2-devel libfdt-devel ninja-build
-  pixman-devel python3 zlib-ng-devel gnupg2
+  pixman-devel python3 zlib-ng-devel gnupg2 patch
 
   # Spice Dependencie(s)
   spice-server
