591769ae67b92b382441659945262891034174d3
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-07-13 10:32:10 +0300

    Update virtualization.sh

--- Hypervisor-Phantom/modules/virtualization.sh
+++ Hypervisor-Phantom/modules/virtualization.sh
@@ -143,3 +143,25 @@ main() {
 }
 
 main
+
+
+
+
+
+# NVRAM Template:
+#
+# sudo pacman -S edk2-ovmf
+#
+# usr/share/edk2/x64/MICROVM.4m.fd
+# usr/share/edk2/x64/OVMF.4m.fd
+# usr/share/edk2/x64/OVMF_CODE.4m.fd
+# usr/share/edk2/x64/OVMF_CODE.secboot.4m.fd
+# usr/share/edk2/x64/OVMF_VARS.4m.fd
+#
+# STORAGE:
+#
+# /var/lib/libvirt/images/
+#
+# Generated firmware from template that is writable:
+#
+# /var/lib/libvirt/qemu/nvram
