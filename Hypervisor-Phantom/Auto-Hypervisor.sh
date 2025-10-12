e543fcf7cf4dac3ef7067be71d5539e6b0da1c0f
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2025-10-11 22:09:35 -0400

    Update Auto-Hypervisor.sh

--- Hypervisor-Phantom/Auto-Hypervisor.sh
+++ Hypervisor-Phantom/Auto-Hypervisor.sh
@@ -52,7 +52,7 @@ detect_distro() {
 }
 
 cpu_vendor_id() {
-  VENDOR_ID=$(LANG=en_US.UTF-8 lscpu 2>/dev/null | awk -F': +' '/^Vendor ID:/ {print $2}' | xargs)
+  VENDOR_ID=$(LC_ALL=C lscpu 2>/dev/null | awk -F': +' '/^Vendor ID:/ {print $2}' | xargs)
 
   if [ -z "$VENDOR_ID" ]; then
     VENDOR_ID=$(awk -F': +' '/vendor_id/ {print $2; exit}' /proc/cpuinfo | xargs) # Fallback method
