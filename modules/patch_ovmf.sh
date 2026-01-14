59c6cc04f4cf0d83de9d687552f1d35505827b71
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-01-14 05:06:53 +0000

    Update patch_ovmf.sh

--- modules/patch_ovmf.sh
+++ modules/patch_ovmf.sh
@@ -2,7 +2,7 @@
 
 [[ -z "$DISTRO" || -z "$LOG_FILE" ]] && { echo "Required environment variables not set."; exit 1; }
 
-source "./utils.sh"
+source ./utils.sh || { echo "Failed to load utilities module!"; exit 1; }
 
 readonly SRC_DIR="$(pwd)/src"
 readonly OUT_DIR="/opt/Hypervisor-Phantom/firmware"
