1d165b81207d7ab882063a27ac872b8c31acdec2
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-01-19 12:00:13 +0000

    Downgrade Linux kernel to version 6.10

--- resources/scripts/arch_kernel_downgrade.sh
+++ resources/scripts/arch_kernel_downgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://archive.archlinux.org/packages/l/linux/linux-6.12.arch1-1-x86_64.pkg.tar.zst
-wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-6.12.arch1-1-x86_64.pkg.tar.zst
+wget https://archive.archlinux.org/packages/l/linux/linux-6.10.arch1-2-x86_64.pkg.tar.zst
+wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-6.10.arch1-2-x86_64.pkg.tar.zst
 
-sudo pacman -U linux-6.12.arch1-1-x86_64.pkg.tar.zst linux-headers-6.12.arch1-1-x86_64.pkg.tar.zst
+sudo pacman -U linux-6.10.arch1-2-x86_64.pkg.tar.zst linux-headers-6.10.arch1-2-x86_64.pkg.tar.zst
 
 sudo sed -i '/^#\[options\]/a IgnorePkg = linux linux-headers' /etc/pacman.conf
 
