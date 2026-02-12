eaa1c83d013f4e27e7294259e953bd3400708e83
Scrut1ny <53458032+Scrut1ny@users.noreply.github.com>
2026-02-12 15:49:05 +0000

    Remove environment variable checks in vfio.sh
    Remove check for required environment variables DISTRO and LOG_FILE.

--- modules/vfio.sh
+++ modules/vfio.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-[[ -z "$DISTRO" || -z "$LOG_FILE" ]] && { echo "Required environment variables not set."; exit 1; }
-
 source ./utils.sh || { echo "Failed to load utilities module!"; exit 1; }
 
 readonly VFIO_CONF_PATH="/etc/modprobe.d/vfio.conf"
